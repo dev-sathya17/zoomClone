29bb6d2b573dd62db822c285e06da5f6df6e072c
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-07-14 17:25:55 +0530

    meeting room UI updated --favicon updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import React, { ReactNode } from "react";
 import StreamClientProvider from "../../providers/StreamClientProvider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Boom",
+  description: "Video Calling App like Zoom",
+  icons: {
+    icon: "/icon/logo.svg",
+  },
+};
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main>
